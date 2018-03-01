3bd60fc6cc83a9b08b4fe5c483254cc9eadfe197
Kukaratza <marioandres.martinez80@gmail.com>
2018-02-28 18:45:50 -0800

    working if statement for comparison

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,7 +7,13 @@
 /* in app.js */
 
 //Global variables.... for now
-//var randQuestion;
+var clickAns = {
+    click: null,
+    right: null
+};
+var randQuestion;
+var correctAnswer;
+var wrongAnswer;
 
 // Create a timer that starts a 60 secs and goes down to zero. 
 function stopWatch() {
@@ -18,18 +24,17 @@ function stopWatch() {
         count = count - 1;
         if (count <= 0) {
             clearInterval(counter);
-            alert("KABOOOM!!!!")
+            //alert("KABOOOM!!!!")
             return;
         }
         document.getElementById("timeR").innerHTML = count + " secs";
     }
 }
 
-
 // create a function that randomizes and inserts QAs to the id questions and answers
 
 function addQAs() {
-    var randQuestion = Math.floor(Math.random() * library.length); //this is a nuber, dont forget!
+    randQuestion = Math.floor(Math.random() * library.length); //this is a nuber, dont forget!
     console.log(randQuestion);
     $("#question").html(library[randQuestion].question);
     $("#answer1").html(library[randQuestion].answer[0]);
@@ -38,12 +43,35 @@ function addQAs() {
 }
 
 // create an onClick event that uses the :checked selector url("https://api.jquery.com/checked-selector/"). 
-
 $("input").on("click", function () {
-    $("#log").html( $("input:checked").val() + " is checked!"); //sanity check, works!
-  })
+    $("#log").html($("input:checked").val() + " is checked!"); //sanity check, works!
+    // if (library.length === 30) return win()
+    //this will be pushed to an array of objects [] = [{key : clickedchioce, key : rightchoice}, {},  {}]
+    var foo = $("input:checked").attr("index");
+    clickAns.click = parseInt(foo);
+    clickAns.right = library[randQuestion].correctIndex;
+    // checkAnswer();
+    console.log(clickAns);
+    checkAnswer();
+});
+
+
+function checkAnswer() {
+    if (clickAns.click === clickAns.right) {
+        correctAnswer++;
+        $("#correct").html(correctAnswer);
+    } else {
+        wrongAnswer++;
+        $("#incorrect").html(wrongAnswer);
+    }
+}
 
 // verify is the clickedAnswer === correctAnswer.
+
+// arr.array.forEach(element => {
+//     if(element.theirchoice === element.correct)
+// });
+
 //all this is if(answer[i] === clickAnswer)
 // var clickAnswer,
 // if (library[0].answer[library[0].correctIndex] === clickAnswer) { 
--- assets/javascript/qas.js
+++ assets/javascript/qas.js
@@ -3,33 +3,127 @@
 
 var library = [
     { //library[0]
-        question: "waht color is the sky",
+        question: "what color is the sky",
         answer: ["red", "yellow", "blue"],
         correctIndex: 2,
     }, { //library[1]
         question: "Question 2",
         answer: ["answer 1", "answer2", "answer3"],
-        correct: 0,
+        correctIndex: 0,
     }, { //library[2]
         question: "Question 3",
         answer: ["answer 1", "answer2", "answer3"],
-        correct: 1,
+        correctIndex: 1,
     }, { //library[3]
         question: "Question 4",
         answer: ["answer 1", "answer2", "answer3"],
-        correct: 2,
+        correctIndex: 2,
     }, { //library[4]
         question: "Question 5",
         answer: ["answer 1", "answer2", "answer3"],
-        correct: 0,
+        correctIndex: 0,
     }, { //library[5]
         question: "Question 6",
         answer: ["answer 1", "answer2", "answer3"],
-        correct: 1,
-    }   
+        correctIndex: 1,
+    }, { //library[6]
+        question: "what color is the sky",
+        answer: ["red", "yellow", "blue"],
+        correctIndex: 2,
+    }, { //library[7]
+        question: "Question 2",
+        answer: ["answer 1", "answer2", "answer3"],
+        correctIndex: 0,
+    }, { //library[9]
+        question: "Question 3",
+        answer: ["answer 1", "answer2", "answer3"],
+        correctIndex: 1,
+    }, { //library[10]
+        question: "Question 4",
+        answer: ["answer 1", "answer2", "answer3"],
+        correctIndex: 2,
+    }, { //library[11]
+        question: "Question 5",
+        answer: ["answer 1", "answer2", "answer3"],
+        correctIndex: 0,
+    }, { //library[12]
+        question: "Question 6",
+        answer: ["answer 1", "answer2", "answer3"],
+        correctIndex: 1,
+    },{ //library[13]
+        question: "what color is the sky",
+        answer: ["red", "yellow", "blue"],
+        correctIndex: 2,
+    }, { //library[14]
+        question: "Question 2",
+        answer: ["answer 1", "answer2", "answer3"],
+        correctIndex: 0,
+    }, { //library[15]
+        question: "Question 3",
+        answer: ["answer 1", "answer2", "answer3"],
+        correctIndex: 1,
+    }, { //library[16]
+        question: "Question 4",
+        answer: ["answer 1", "answer2", "answer3"],
+        correctIndex: 2,
+    }, { //library[17]
+        question: "Question 5",
+        answer: ["answer 1", "answer2", "answer3"],
+        correctIndex: 0,
+    }, { //library[18]
+        question: "Question 6",
+        answer: ["answer 1", "answer2", "answer3"],
+        correctIndex: 1,
+    },{ //library[19]
+        question: "what color is the sky",
+        answer: ["red", "yellow", "blue"],
+        correctIndex: 2,
+    }, { //library[20]
+        question: "Question 2",
+        answer: ["answer 1", "answer2", "answer3"],
+        correctIndex: 0,
+    }, { //library[21]
+        question: "Question 3",
+        answer: ["answer 1", "answer2", "answer3"],
+        correctIndex: 1,
+    }, { //library[22]
+        question: "Question 4",
+        answer: ["answer 1", "answer2", "answer3"],
+        correctIndex: 2,
+    }, { //library[23]
+        question: "Question 5",
+        answer: ["answer 1", "answer2", "answer3"],
+        correctIndex: 0,
+    }, { //library[24]
+        question: "Question 6",
+        answer: ["answer 1", "answer2", "answer3"],
+        correctIndex: 1,
+    }, { //library[25]
+        question: "what color is the sky",
+        answer: ["red", "yellow", "blue"],
+        correctIndexIndex: 2,
+    }, { //library[26]
+        question: "Question 2",
+        answer: ["answer 1", "answer2", "answer3"],
+        correctIndex: 0,
+    }, { //library[27]
+        question: "Question 3",
+        answer: ["answer 1", "answer2", "answer3"],
+        correctIndex: 1,
+    }, { //library[28]
+        question: "Question 4",
+        answer: ["answer 1", "answer2", "answer3"],
+        correctIndex: 2,
+    }, { //library[29]
+        question: "Question 5",
+        answer: ["answer 1", "answer2", "answer3"],
+        correctIndex: 0,
+    }, { //library[30]
+        question: "Question 6",
+        answer: ["answer 1", "answer2", "answer3"],
+        correctIndex: 1,
+    },  
 ]
-
-
 
 
 
